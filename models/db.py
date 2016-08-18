cca28603b40f1457d13fb6f08f568f9b81590153
yogurtyogurt <siero@virgilio.it>
2016-08-18 08:15:35 +0200

    added sqlite branch

--- models/db.py
+++ models/db.py
@@ -17,7 +17,7 @@ myconf = AppConfig(reload=True)
 
 if not request.env.web2py_runtime_gae:
     ## if NOT running on Google App Engine use SQLite or other DB
-    db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'])
+    db = DAL(myconf.take('db.uri'), pool_size=10, check_reserved=['all'])
 else:
     ## connect to Google BigTable (optional 'google:datastore://namespace')
     db = DAL('google:datastore+ndb')
